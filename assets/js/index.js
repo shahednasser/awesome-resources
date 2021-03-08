9cd7fc87c6fcab5f933d34541c3ec307cf74cfce
Korak Kurani <kurani@comcard.de>
2021-03-08 12:51:34 +0100

    Toggle Theme funtionality
    - Added toggle theme function which toggles between dark and light theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,30 @@ function toggleScrollButton() {
   }
 }
 
+// color variables
+const lightBlack = '#3e4040'
+const darkBlack = '#252626'
+const mainColor = '#75C9C8'
+const footerColor = '#f9f9f9'
+
+function toggleTheme(){
+  if(document.getElementById('toggleCheckBox').checked){
+    document.body.style.background = `${darkBlack}`
+    document.body.style.color = 'snow'
+    header.style.background = `${lightBlack}`
+    footer.style.background = `${lightBlack}`
+  }else{
+    document.body.style.background = 'snow'
+    document.body.style.color = 'black'
+    header.style.background = `${mainColor}`
+    footer.style.background = `${footerColor}`
+  }
+}
+
 $(document).ready(function () {
+  const header = document.getElementById('header')
+  const footer = document.getElementById('footer')
   $("#scroll-up").click(scrollToTop);
   $(window).scroll(toggleScrollButton);
+  $('#toggleCheckBox').change(toggleTheme)
 });
